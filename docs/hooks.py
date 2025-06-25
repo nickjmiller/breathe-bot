a3f0f02742275fc9a67c7cfddde2e22dfc675d3d
Nick Miller <mail@nickmiller.dev>
2025-06-25 20:41:21 +0000

    Simplify folder structure in docs

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,9 +1,7 @@
-import os
 import shutil
 
 
 def on_startup(*args, **kwargs):
     shutil.copy("README.md", "docs/index.md")
     # Copy logo
-    os.makedirs("docs/assets", exist_ok=True)
     shutil.copy("assets/logo_256.png", "docs/assets/logo_256.png")
