9a02e086faf35b595a844c8523980f9d1bc611ad
Nick Miller <mail@nickmiller.dev>
2025-06-25 19:43:39 +0000

    Make assets dir if not exist for mkdocs

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,7 +1,9 @@
+import os
 import shutil
 
 
 def copy_readme(*args, **kwargs):
     shutil.copy("README.md", "docs/index.md")
     # Copy logo
+    os.makedirs("docs/assets", exist_ok=True)
     shutil.copy("assets/logo_256.png", "docs/assets/logo_256.png")
