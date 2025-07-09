4a7c4afc92a81a911c55b8759057c493393f3ab0
Nick Miller <mail@nickmiller.dev>
2025-07-09 17:02:34 +0000

    Add unit tests for voice

---
+++ tests/test_voice.py
@@ -0,0 +1,11 @@
+from src.voice import Voice, get_durations
+
+
+def test_voice_has_display_name():
+    for voice in Voice:
+        assert voice.display_name is not None
+
+
+def test_voice_has_durations():
+    for voice in Voice:
+        assert get_durations(voice) is not None
